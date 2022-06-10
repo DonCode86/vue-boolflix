4a2353abae7a6012420d9ec65ca3b293dedc06d3
DonCode <99022919+DonCode86@users.noreply.github.com>
2022-06-10 10:53:57 +0200

    aggiunta locandina

--- src/components/sections/SectionFilms.vue
+++ src/components/sections/SectionFilms.vue
@@ -2,6 +2,10 @@
   <section class="film-section">
     <ul>
       <li v-for="film in SelectedFilm.films" :key="film.id">
+        <img
+          :src="'https://image.tmdb.org/t/p/original/' + film.poster_path"
+          alt=""
+        />
         {{ film.title }}, {{ film.original_title }},
         {{ film.original_language }}, {{ film.vote_average }}
       </li>
@@ -21,4 +25,8 @@ export default {
 }
 </script>
 
-<style></style>
+<style>
+img {
+  width: 18.75rem;
+}
+</style>
